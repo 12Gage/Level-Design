d890978d0f17cc23978a6568b3dd66ed72603789
12Gage <glhabiger@mymail.tstc.edu>
2016-06-03 19:51:30 -0500

    unity

---
+++ Unity Project/Assets/Scripts/gateScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gateScript : MonoBehaviour {
+
+    public Animator gate;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetButton("Fire2"))
+            {
+                gate.SetTrigger("Open");
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
